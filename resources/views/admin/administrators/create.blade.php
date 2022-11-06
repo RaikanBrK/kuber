7ee6abbf5f0c9e25f8d32016cc9f6c8a71f82756
Carlos Alexandre <carlos24106@outlook.com>
2022-11-06 08:57:13 -0300

    Melhorando responsividade dos formulários

--- resources/views/admin/administrators/create.blade.php
+++ resources/views/admin/administrators/create.blade.php
@@ -13,21 +13,21 @@
             @csrf
 
             <div class="form-row">
-                <div class="form-group col-md-6">
+                <div class="form-group col-sm-6">
                     <label for="name">Nome</label>
                     <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
                 </div>
-                <div class="form-group col-md-6">
+                <div class="form-group col-sm-6">
                     <label for="email">Email</label>
                     <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
                 </div>
             </div>
             <div class="form-row">
-                <div class="form-group col-md-4">
+                <div class="form-group col-sm-5 col-md-4">
                     <label for="password">Senha</label>
                     <input type="password" class="form-control" id="password" name="password">
                 </div>
-                <div class="form-group col-md-4">
+                <div class="form-group col-sm-5 col-md-4">
                     <label for="password_confirmation">Confirmar Senha</label>
                     <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
                 </div>
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -14,7 +14,7 @@
             <input type="hidden" name="user_id" value="{{ $user->id }}">
 
             <div class="form-row mb-2">
-                <div class="col-auto pr-4">
+                <div class="col-12 col-sm-auto pr-sm-4">
                     <div class="form-group">
                         <label for="campoImageProfile" id="labelImageProfile" class="d-flex flex-column align-items-center">
                             <img src="{{ $user->image() }}" alt="{{ $user->name }}" class="img-fluid" id="imageProfile" accept=".png, .jpg, .jpeg">
@@ -25,11 +25,11 @@
                 </div>
                 <div class="col">
                     <div class="form-row">
-                        <div class="form-group col">
+                        <div class="form-group col-12 col-sm-6">
                             <label for="name">Nome</label>
                             <input type="text" class="form-control" id="name" name="name" placeholder="{{ $user->name }}" value="{{ $user->name }}">
                         </div>
-                        <div class="form-group col">
+                        <div class="form-group col-12 col-sm-6">
                             <label for="email">Email</label>
                             <input type="email" class="form-control" id="email" name="email" placeholder="{{ $user->email }}" value="{{ $user->email }}">
                         </div>
